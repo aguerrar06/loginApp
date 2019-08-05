657314f809c9f2190e37ea89ed82e825d5429c4b
aguerrar06 <aguerrar06@gmail.com>
2019-08-05 22:43:21 +0200

    Configuración de Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPagJJRVg-jnobgxiVCIpBuCugW36WVgI",
+    authDomain: "loginapp-b986e.firebaseapp.com",
+    databaseURL: "https://loginapp-b986e.firebaseio.com",
+    projectId: "loginapp-b986e",
+    storageBucket: "",
+    messagingSenderId: "79594377892",
+    appId: "1:79594377892:web:e7960db2c292d682"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPagJJRVg-jnobgxiVCIpBuCugW36WVgI",
+    authDomain: "loginapp-b986e.firebaseapp.com",
+    databaseURL: "https://loginapp-b986e.firebaseio.com",
+    projectId: "loginapp-b986e",
+    storageBucket: "",
+    messagingSenderId: "79594377892",
+    appId: "1:79594377892:web:e7960db2c292d682"
+  }
 };
 
 /*
