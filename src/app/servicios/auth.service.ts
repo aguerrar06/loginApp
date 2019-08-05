9985f7676a53886610798d0e31657ad9f901cac6
aguerrar06 <aguerrar06@gmail.com>
2019-08-06 00:15:55 +0200

    Servicio Auth creado y el registro de usuarios

---
+++ src/app/servicios/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { resolve, reject } from 'q';
+import 'rxjs/add/operator/map';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    public afAuth: AngularFireAuth
+  ) { }
+
+  userRegister(email: string, pass: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.createUserWithEmailAndPassword(email, pass).then(userData => resolve(userData), err => reject(err));
+    });
+  }
+
+  userLogin(email: string, pass: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.signInWithEmailAndPassword(email, pass).then(userData => resolve(userData), err => reject(err));
+    });
+  }
+
+  getAuth() {
+    return this.afAuth.authState.map(auth => auth);
+  }
+
+  logout() {
+    return this.afAuth.auth.signOut();
+  }
+
+}
